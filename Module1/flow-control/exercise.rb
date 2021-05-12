14a360ba0d1303b4e87869ca4201376d8a29154c
Fernando Soto <fernando@icalialabs.com>
2021-05-11 18:11:59 -0600

    first commit

--- Module1/flow-control/exercise.rb
+++ Module1/flow-control/exercise.rb
@@ -22,7 +22,15 @@
 # Exercise 1:
 # Given an array of integers print when is odd and when is even
 # Hint: You need to use one of the loops from the lesson
-integers = (1..100).to_a.sample
+puts "Exercise 1:"
+integers = (1..100).to_a
+
+i = 0
+while i <= 99
+  puts integers[i] % 2 == 0 ? "#{i+1} => even" : "#{i+1} => odd"
+  i+=1
+end
+puts "\n"
 
 # Exercise 2:
 # You get to throw a dice, print the following for each value:
@@ -32,19 +40,39 @@ integers = (1..100).to_a.sample
 # 4 - Fantastic Four
 # 5 - 5 seconds to...
 # 6 - Number of first Avengers
+puts "Exercise 2:"
 dice = [1,2,3,4,5,6]
-dice_result = dice.sample
+
+i = 0
+while i <= 5
+  if dice[i] == 1
+    puts "1 - You only live once"
+  elsif dice[i] == 2
+    puts "2 - You have two hands"
+  elsif dice[i] == 3
+    puts "3 - March is the third month of the year"
+  elsif dice[i] == 4
+    puts "4 - Fantastic Four"
+  elsif dice[i] == 5
+    puts "5 - 5 seconds to..."
+  elsif dice[i] == 6
+    puts "6 - Number of first Avengers"
+  end
+  i+=1
+end
+puts "\n"
 
 # Exercise 3:
+puts "Exercise 3: ?"
 # Given the following code:
-# 1. What is the output from the first condition
-# 2. What is the output from the second condition
-# 3. What is the output from the third condition
+# 1. What is the output from the first condition => puts "There is plenty of cars for everyone"
+# 2. What is the output from the second condition => "Four wheels are safer"
+# 3. What is the output from the third condition => "Maybe we split?"
 # 4. Change the values for people, cars and motorcycles:
 #    4.1 people = 20, cars = 10, motorcycles = 30
-people = 30
-cars = 40
-motorcyles = 15
+people = 20
+cars = 10
+motorcyles = 30
 
 if cars > people
   puts "There is plenty of cars for everyone"
@@ -62,23 +90,85 @@ else
   puts "Is there any other out?"
 end
 
-if people > motorcycles
+if people > motorcyles
   puts "Maybe we split?"
 else
   puts "Everyone gets a bike"
 end
 
+puts "\n"
 # Exercise 4: 
 # Given an string:
-# 1. print the words that have 2 letters, following this format - The word 'On' has two words 
+# 1. print the words that have 2 letters, following this format - The word 'On' has two words (letters?) 
 # 2. print the words that have more than to 2 letters and less or equal to 5 letters, following this format - The word 'man' has thre words
 # 3. print the words that have more than 5 letters, following this format - The word 'offering' has more than 5 words 
+puts "Exercise 4: "
 phrase = "On offering to help the blind man, the man who then stole his car, had not, at that precise moment, had any evil intention, quite the contrary, what he did was nothing more than obey those feelings of generosity and altruism which, as everyone knows, are the two best traits of human nature and to be found in much more hardened criminals than this one, a simple car-thief without any hope of advancing in his profession, exploited by the real owners of this enterprise, for it is they who take advantage of the needs of the poor"
 
+words = phrase.split(" ")
+
+i = 0
+while i <= words.length
+  puts "The word '#{words[i]}' has two letters" if words[i].to_s.size == 2
+  i+=1
+end
+
+puts "\n"
+
+i = 0
+while i <= words.length
+  if words[i].to_s.size > 2 && words[i].to_s.size <= 5
+    if words[i].to_s.size == 3
+      puts "The word '#{words[i]}' has three letters"
+    elsif words[i].to_s.size == 4
+      puts "The word '#{words[i]}' has four letters"
+    else
+      puts "The word '#{words[i]}' has five letters"
+    end
+  end 
+  i+=1
+end
+
+puts "\n"
+
+i = 0
+while i <= words.length
+  puts "The word '#{words[i]}' more than 5 words" if words[i].to_s.size > 5
+  i+=1
+end
+
+
+puts "\n"
 # Exercise 5:
 # Using a `times` iterator print the even numbers from a given number
+puts "Exercise 5:"
 number = 100
 
+number.times do |n|
+  puts n % 2 == 0 ? "#{n} => even" : "#{n} => odd"
+end
+
+puts "\n"
 # Exercise 6:
 # Using a `times`, `while` and `until` loops print "Beetlejuice!" three times
+
+puts "Exercise 6:"
+
+3.times { puts "Beetlejuice!" }
+
+puts "\n"
+
+i = 0
+while i < 3
+  puts "Beetlejuice!"
+  i+=1
+end
+
+puts "\n"
+
+i = 0
+until i == 3
+  puts "Beetlejuice!"
+  i+=1
+end
 
